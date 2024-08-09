ea04aab30fbbba787bd4eed01b8b57cf6ec42e8f
ThallesCanela <thalles_tsc@hotmail.com>
2024-08-09 10:00:56 -0300

    Alteração na estrutura para facilitar a clonagem do repositorio

--- 2023/Semestre 2/ARA0078 - COMPUTAÇÃO EM NUVEM E WEB SERVICES EM LINUX/Scripts/Agendamento/debian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Caminho para o script monitor_debian.sh
-SCRIPT_PATH="/path/to/your/monitor_debian.sh"
-
-# Adicione uma entrada cron para executar o script todos os dias às 12:00
-(crontab -l 2>/dev/null; echo "0 12 * * * $SCRIPT_PATH") | crontab -
